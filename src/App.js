bcae77855bad2d95122a0e328dd9a768e9b85a63
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2021-12-17 10:39:02 +0530

    added login page and register page to the blog site

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-//import Home form "./pages/home/Home";
+import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar.jsx"
+import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
 import Setting from "./pages/setting/Setting.jsx";
-//import Single from "./pages/single/Single.jsx";
-//import Write from "./pages/write/Write.jsx";
+import Single from "./pages/single/Single.jsx";
+import Write from "./pages/write/Write.jsx";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      <TopBar/>
-      <Setting/>
-    </div>
+    <Router>
+      <TopBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/write">
+          <Write />
+        </Route>
+        <Route path="/setting">
+          <Setting />
+        </Route>
+        <Route path="/Post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
