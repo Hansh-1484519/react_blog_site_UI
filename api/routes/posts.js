994a60f56df0f3970182f67c673b0ece192f9679
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2021-12-21 22:48:30 +0530

    added get api in posts.js to fetch all the posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -61,5 +61,26 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+//Get All Posts
+router.get("/" , async (req , res) => {
+    const username = req.query.User;
+    const catName = req.query.cat;
+    try{
+        let posts;
+        if(username){
+            posts = await Post.find({username});
+        } else if( catName ){
+            posts = await Post.find({ categories : {
+                $in: [catName],},
+            });
+        }else{
+            posts = await Post.find();
+        }
+        res.status(200).json(posts);
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router
