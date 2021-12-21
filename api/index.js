f9b2ce085e9d870f57f888a690c6cd22afdbb784
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2021-12-21 15:59:06 +0530

    added new api folder and other files

---
+++ api/index.js
@@ -0,0 +1,28 @@
+//import express from 'express'
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("connected to MongoDB"))
+.catch((err) => console.log(err));
+
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+// This is not good practice to write these operations urls inside the index.js file 
+
+//app.use('/', (req , res) => {
+//    console.log('Hey, your server is listening to browser');
+//})
+
+app.listen("5000" , ()=>{
+    console.log('This is my backend server');
+});
