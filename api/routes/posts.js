4a016ed111a1ad92969f8696c95e84e694ee7431
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2021-12-21 17:20:20 +0530

    added new put api in posts.js to update the posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -15,6 +15,25 @@ router.post("/" , async ( req , res) => {
 })
 
 // Update Posts
+router.put("/:id", async (req , res) => {
+    const post = await Post.findById(req.params.id);
+    if( post.username === req.body.username){
+        try{
+            const updatedPost = await Post.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set : req.body
+                },
+                { new : true}
+            );
+            res.status(200).json(updatedPost);
+        } catch(err){
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(401).json("You can update only your posts");
+    }
+})
 
 // Delete User ( this delete api can now delete any user but update it so that a person can delete himself only)
 
