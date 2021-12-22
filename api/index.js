33df43fbc5fb8606a14d2de93d196147e697a603
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2021-12-22 11:23:59 +0530

    added categories routes in index.js file and updated categories.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
+const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -18,7 +19,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-
+app.use("/api/categories", categoryRoute);
 // This is not good practice to write these operations urls inside the index.js file 
 
 //app.use('/', (req , res) => {
