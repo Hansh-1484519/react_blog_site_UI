4b9c2912a6f22d64eceebe8fd730b0911cd7074a
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2021-12-26 22:27:38 +0530

    updated Write.jsx file for posting

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,18 +1,65 @@
 import "./write.css"
+import axios from "axios"
+import { useContext, useState } from "react";
+import { Context } from "../../context/Context"
 
 export default function Write() {
+    const [title , setTitle] = useState("");
+    const [desc , setDesc] = useState("");
+    const [file , setfile] = useState(null);
+    const {user} = useContext(Context);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newPost = {
+            username : user.username,
+            title,
+            desc
+        }
+
+        if( file ){
+            const data = new FormData();
+            const filename = Date.now() + file.name;
+            data.append( "name" , filename);
+            data.append( "file" , file);
+            newPost.photo = filename;
+            try{
+                await axios.post("/upload" , data);
+
+            } catch ( err ) {
+                console.log(err);
+            }
+        }
+        try{
+            const res = await axios.post("/posts", newPost);
+            window.location.replace("/post/" + res.data._id);
+        } catch(err){
+            console.log(err);
+        }
+
+    }
+
+
     return (
         <div className="write">
+
+            {file && (
             <img
             className="writeImg" 
-            src="https://thumbs.dreamstime.com/b/magical-mountain-landscape-great-view-pizes-de-cir-ridge-valley-gardena-national-park-dolomites-south-tyrol-location-185631959.jpg" alt="Desktop" />
-            <form className="writeForm">
+            src = { URL.createObjectURL(file)} alt="Desktop" />
+            )}
+            
+            <form className="writeForm" onSubmit = {handleSubmit}>
                 <div className="writeFormGroup">
                     <label htmlFor="fileInput">
                         <i className="writeIcon fas fa-plus"></i>
                     </label>
-                    <input type="file" id="fileInput" style={{ display: "none" }} />
-                    <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
+                    <input type="file" id="fileInput" style={{ display: "none" }} 
+                    onChange = { (e) => setfile( e.target.files[0])}
+                    />
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus={true} 
+                    onChange = { (e) => setTitle( e.target.value)}
+                    />
                 </div>
 
                 <div className="writeFormGroup">
@@ -20,9 +67,10 @@ export default function Write() {
                     placeholder="Tell your story !" 
                     type="text"
                     className="writeInput writeText"
+                    onChange = { e => setDesc( e.target.value)}
                     ></textarea>
                 </div>
-                <button className="writeSubmit">Publish</button>
+                <button className="writeSubmit" type = "submit">Publish</button>
             </form>
 
         </div>
