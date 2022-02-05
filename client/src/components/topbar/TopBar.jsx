23129e070e4b99b313e0b503d7db99a84dbc748a
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-02-05 07:13:34 +0530

    updated topbar and change code styling

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -1,53 +1,70 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
-import "./topbar.css"
+import "./topbar.css";
 
 export default function TopBar() {
-    const {user , dispatch } = useContext( Context);
+  const { user, dispatch } = useContext(Context);
 
-    const handleLogout = () => {
-        dispatch( { type : "LOGOUT" } );
-    }
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
 
-
-    return (
-        <div className="top">
-            <div className="topLeft">
-                <i className="topIcon fab fa-facebook-square"></i>
-                <i className="topIcon fab fa-twitter-square"></i>
-                <i className="topIcon fab fa-pinterest-square"></i>
-                <i className="topIcon fab fa-instagram-square"></i>
-            </div>
-            <div className="topCenter">
-                <ul className="topList">
-                    <li className="topListItem"><Link className = "link" to = "/">HOME</Link></li>
-                    <li className="topListItem"><Link className = "link" to = "/about">ABOUT</Link></li>
-                    <li className="topListItem"><Link className = "link" to = "/contact">CONTACT</Link></li>
-                    <li className="topListItem"><Link className = "link" to = "/write">WRITE</Link></li>
-                    <li className="topListItem" onClick = {handleLogout} >{ user && "LOGOUT"}</li>
-
-                </ul>
-            </div>
-            <div className="topRight">
-                { user ? (
-                    <img 
-                    className="topImg"
-                    src= { user.profilePic }
-                    alt="profile" />   
-
-                ) : (
-                    <ul className = "topList">
-                        <li className = "topListItem">
-                            <Link className = "link" to = "/login">LOGIN</Link>
-                        </li>
-                        <li className = "topListItem"> 
-                            <Link className = "link" to = "/register"> REGISTER</Link> 
-                        </li>
-                    </ul>
-                )}
-                <i className="topSearchIcon fas fa-search"></i>   
-            </div>
-        </div>
-    )
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/about">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/contact">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "LOGOUT"}
+          </li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <img className="topImg" src={user.profilePic} alt="profile" />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                {" "}
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
 }
