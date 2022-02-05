087368a1833b6e33e5822aa553732bc9b8aef297
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-02-05 08:12:45 +0530

    added new posts to blog

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -13,7 +13,7 @@ router.post('/register' , async (req , res) => {
             password : hashedPassword,
         });
         const user = await newUser.save();
-        res.status(200).json("hello");
+        res.status(200).json(user);
     } catch(err){
         res.status(500).json(err);
     }
