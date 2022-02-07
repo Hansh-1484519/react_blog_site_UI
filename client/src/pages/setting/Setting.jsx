553c7bacb86d8023857d479445b89387f057c599
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-02-07 07:54:22 +0530

    updated settings page

--- client/src/pages/setting/Setting.jsx
+++ client/src/pages/setting/Setting.jsx
@@ -1,9 +1,38 @@
 import "./setting.css"
 import Sidebar from "../../components/sidebar/Sidebar"
 import { Context } from "../../context/Context"
-import { useContext } from "react"
+import { useContext, useState } from "react"
+import axios from "axios";
 export default function Setting() {
+
+    const [file , setfile] = useState(null);
+    const [username , setUsername] = useState("");
+    const [email , setEmail] = useState("");
+    const [password , setPassword] = useState("");
     const {user} = useContext(Context);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const updatedUser = {
+            userId : user._id,
+            username,email,password
+        }
+        if( file ){
+            const data = new FormData();
+            const filename = Date.now() + file.name;
+            data.append( "name" , filename);
+            data.append( "file" , file);
+            updatedUser.profilePic = filename;
+            try{
+                await axios.post("/upload" , data)
+            } catch ( err ) {
+                console.log(err);
+            }
+        }
+        try{
+            await axios.put("/users"+user._id, updatedUser);
+        } catch(err){
+            console.log(err);
+        }}
     return (
         <div className="setting">
             <div className="settingWrapper">
@@ -26,7 +55,7 @@ export default function Setting() {
                     <input type="email" placeholder="hansh789@gmail.com"/>
                     <label>Password</label>
                     <input type="password"/>
-                    <button className="settingSubmit">Update</button>
+                    <button className="settingSubmit" onClick={handleSubmit}>Update</button>
                 </form>
 
             </div>
